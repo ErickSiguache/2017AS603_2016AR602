e113112820f43255864d91046b0b15af037dc8c3
ErickSiguache <erickaguilars403@gmail.com>
2021-10-09 10:19:10 -0600

    Creacion del proyecto

---
+++ 2017AS603_2016AR602/2017AS603_2016AR602Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using _2017AS603_2016AR602.Models;
+
+namespace _2017AS603_2016AR602
+{
+    public class _2017AS603_2016AR602Context : DbContext
+    {
+        public _2017AS603_2016AR602Context(DbContextOptions<_2017AS603_2016AR602Context> options) : base(options)
+        {
+        }
+    }
+}
